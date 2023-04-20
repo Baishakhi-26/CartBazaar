b85ab9a29f15a51100d8a92372bc6a3cd1d0f234
Arijit-Kumar-Pandab <arijitkuamrpandab@gmail.com>
2023-04-20 23:19:23 +0530

    implemented LoginActivity.kt, OTPActivity.kt and RegisterActivity.kt

--- app/src/main/java/com/example/cartbazaar/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/cartbazaar/activity/RegisterActivity.kt
@@ -1,12 +1,72 @@
 package com.example.cartbazaar.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.cartbazaar.R
+import com.example.cartbazaar.databinding.ActivityRegisterBinding
+import com.example.cartbazaar.model.UserModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInButton.setOnClickListener {
+            openLogin()
+        }
+
+        binding.signUpButton.setOnClickListener {
+            validateUser()
+        }
+    }
+
+    private fun validateUser() {
+        if (binding.userNameInput.text!!.isEmpty() || binding.userNumberInput.text!!.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        } else {
+            storeData()
+        }
+    }
+
+    private fun storeData() {
+        val builder = AlertDialog.Builder(this)
+            .setTitle("Loading....")
+            .setMessage("Please Wait")
+            .setCancelable(false)
+            .create()
+        builder.show()
+
+        val preferences = this.getSharedPreferences("user", MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putString("number", binding.userNumberInput.text.toString())
+        editor.putString("name", binding.userNameInput.text.toString())
+        editor.apply()
+
+        val data = UserModel(userName = binding.userNameInput.text.toString(),
+            userPhoneNumber = binding.userNumberInput.text.toString())
+
+        Firebase.firestore.collection("users").document(binding.userNumberInput.text.toString())
+            .set(data).addOnSuccessListener {
+                Toast.makeText(this, "User registered", Toast.LENGTH_SHORT).show()
+                builder.dismiss()
+                openLogin()
+            }
+            .addOnFailureListener {
+                builder.dismiss()
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun openLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
